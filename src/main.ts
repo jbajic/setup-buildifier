2bcc53f6a3f076c3b2ae2727cb588cc36d9bd725
Jure Bajic <jure.bajic94@gmail.com>
2022-10-11 06:46:18 +0200

    Setup tests (#2)

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,24 @@
-import * as cache from '@actions/cache';
-import * as core from '@actions/core';
-import * as installer from './installer';
-const cachedir = require('cachedir');
+import * as cache from "@actions/cache";
+import * as core from "@actions/core";
+import * as installer from "./installer";
+
+const cachedir = require("cachedir");
 
 export async function run() {
   try {
-    const versionSpec = core.getInput('buildifier-version');
-    const token = core.getInput('token');
+    const versionSpec = core.getInput("buildifier-version");
+    const token = core.getInput("token");
 
     const installDir = await installer.getBuildifier(versionSpec, token);
     core.addPath(installDir);
-    core.info('Added buildifier to the path');
+    core.info("Added buildifier to the path");
 
-    const cacheDir: string = cachedir('buildifier');
-    await cache.restoreCache([cacheDir], 'buildifier');
+    const cacheDir: string = cachedir("buildifier");
+    await cache.restoreCache([cacheDir], "buildifier");
     core.info(`Restored buildifier cache dir @ ${cacheDir}`);
 
     core.info(`Successfully setup buildifier version ${versionSpec}`);
   } catch (e: any) {
-      core.setFailed(e.message);
+    core.setFailed(e.message);
   }
 }
